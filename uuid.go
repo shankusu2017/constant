93b5ee0316337fe506f32fcfe73b473928a1d759
shankusu <shankusu2017@gmail.com>
2024-05-08 11:49:45 +0800

    little

--- uuid.go
+++ uuid.go
@@ -1,6 +1,6 @@
 package constant
 
 const (
-	// 一台机器的uuid的byte的长度(用于网络传输，实际存在文件中是以hex-string的方式，长度为2倍)
-	UUID_MACHINE_LENGHT = 8 // 单位 byte
+	// UUID_MACHINE_LENGTH 一台机器的uuid的byte的长度(用于网络传输，实际存在文件中是以hex-string的方式，长度为2倍)
+	UUID_MACHINE_LENGTH = 8 // 单位 byte
 )
