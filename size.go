2855ee18c21353d00b52eb51a244f87c6ea2b58e
shankusu <shankusu2017@gmail.com>
2024-05-14 10:12:55 +0800

    little

--- size.go
+++ size.go
@@ -2,8 +2,17 @@ package constant
 
 const (
 	Size256      = 256
+	Size512      = 512
 	Size1K       = 1024
-	Size256K     = Size1K * 256
-	Size1M       = Size256K * 4
+	Size2K       = 2 * Size1K
+	Size4K       = 4 * Size1K
+	Size8K       = 8 * Size1K
+	Size16K      = 16 * Size1K
+	Size32K      = 32 * Size1K
+	Size64K      = 64 * Size1K
+	Size128K     = 128 * Size1K
+	Size256K     = 256 * Size1K
+	Size512K     = 512 * Size1K
+	Size1M       = 1024 * Size1K
 	SizeByteBits = 8 // 1 Byte == 8Bit
 )
