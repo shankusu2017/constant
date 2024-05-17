7bda3c95181d3a2f970cf58eea661005244bf74b
shankusu <shankusu2017@gmail.com>
2024-05-17 22:59:35 +0800

    little

--- time.go
+++ time.go
@@ -3,7 +3,8 @@ package constant
 import "time"
 
 const (
-	Time30Min     = time.Minute * 30
-	TimePing      = time.Second * 1
-	TimeKeepAlive = time.Second * 7
+	Time30Min       = time.Minute * 30
+	TimePing        = time.Second * 1
+	TimeKeepAlive   = time.Second * 7
+	TimeScanSession = time.Minute * 10
 )
