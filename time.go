0f0c63e5b9be8ef7d2e7848243622cf10d56b9cf
shankusu <shankusu2017@gmail.com>
2024-05-18 11:22:47 +0800

    little

--- time.go
+++ time.go
@@ -3,9 +3,10 @@ package constant
 import "time"
 
 const (
-	Time30Min       = time.Minute * 30
-	TimePing        = time.Second * 5 /* 多久 ping 一次 */
-	TimePingRound   = time.Minute * 2 /* 一轮 ping 持续多久 */
-	TimeKeepAlive   = time.Minute * 1
-	TimeScanSession = time.Minute * 10
+	Time30Min            = time.Minute * 30
+	TimePing             = time.Second * 5   /* 多久 ping 一次 */
+	TimePingRound        = time.Minute * 2   /* 一轮 ping 持续多久 */
+	TimeKeepAlive        = time.Minute * 1   /* node 向 nodeMgr 发送心态的间隔 */
+	TimeHeartbeatTimeOut = TimeKeepAlive * 7 /* nodeMgr 认定 node 掉线的超时时间 */
+	TimeScanSession      = time.Minute * 10
 )
