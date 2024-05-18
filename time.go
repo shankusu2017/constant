32f2574c4e98b6acb3335c2c61c3410caf5125cc
shankusu <shankusu2017@gmail.com>
2024-05-18 09:12:52 +0800

    little

--- time.go
+++ time.go
@@ -4,7 +4,8 @@ import "time"
 
 const (
 	Time30Min       = time.Minute * 30
-	TimePing        = time.Second * 1
-	TimeKeepAlive   = time.Second * 7
+	TimePing        = time.Second * 5 /* 多久 ping 一次 */
+	TimePingRound   = time.Minute * 2 /* 一轮 ping 持续多久 */
+	TimeKeepAlive   = time.Minute * 1
 	TimeScanSession = time.Minute * 10
 )
